9f40e8770527e7bfcc6e8eafcf3881c638872f46
mayconmoraes-dm <mayconmoraes.dm@gmail.com>
2019-10-16 19:51:53 -0300

    inicial commit

---
+++ core/classes/Teste.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace \Core\Classes;
+
+class Teste
+{
+    public function metodo1()
+    {
+        echo 'Metodo 1';
+    }
+
+    public function metodo2()
+    {
+        echo 'Metodo 2';
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+$teste = new \Core\Classes\Teste;
+
